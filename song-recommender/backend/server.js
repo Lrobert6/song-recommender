87234f4ff0ca2bcd1a07b9d01fecb1fd61ebba5a
Lrobert6 <lrobert6@nait.ca>
2025-01-15 21:35:05 -0700

    Updated Env

--- song-recommender/backend/server.js
+++ song-recommender/backend/server.js
@@ -20,8 +20,8 @@ let accessToken = null;
 let tokenExpiresAt = null;
 
 // Supabase Configuration
-const supabaseUrl = process.env.REACT_APP_API_URL;
-const supabaseKey = process.env.REACT_APP_API_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Function to get Spotify Access Token
