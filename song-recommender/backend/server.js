909d313ea09ba151a1f6d4c7a1c583dfe0f01e6c
Lrobert6 <lrobert6@nait.ca>
2025-01-15 22:09:33 -0700

    Update server.js

--- song-recommender/backend/server.js
+++ song-recommender/backend/server.js
@@ -6,7 +6,7 @@ const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
